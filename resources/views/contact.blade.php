2cdaa34dfe0200590c74ab0d2ef2af952c82d0a3
Felipe Genuino <design@felipegenuino.com>
2024-03-27 21:40:33 -0300

    day 4

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading> Contact</x-slot:heading>
+    <h1>Hello from my page Contact</h1>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading> Home</x-slot:heading>
+    <h1>Hello from my page Home</h1>
+</x-layout>
